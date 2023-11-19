e26eda579fc19edcd1f01ff6ee4d64987aab969d
Amanda Yu <92806914+yuamanda@users.noreply.github.com>
2023-11-19 18:25:21 -0500

    added unit tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,41 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import { render, screen, fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  const count = screen.getByTestId('count');
+  expect(count.textContent).toBe('0');
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  var count = screen.getByTestId('count');
+  const increment = screen.getByText('+');
+
+  fireEvent.click(increment);
+
+  expect(count.textContent).toBe('1');
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const count = screen.getByTestId('count');
+  const minusButton = screen.getByText('-');
+
+  fireEvent.click(minusButton);
+
+  expect(count).toHaveTextContent('-1');
 });
